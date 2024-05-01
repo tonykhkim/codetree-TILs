3622d402a73164d2d310a825b5aff82711fbaf78
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-02 01:35:58 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240502/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin >> a >> b;
+
+    if (a>0)
+    {
+        for (int i = 1;i<=b;i++)
+        {
+            cout << a;
+        }
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
