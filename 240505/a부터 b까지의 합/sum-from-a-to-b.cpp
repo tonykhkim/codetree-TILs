7efe527abdd7de53c81efff83a96dca8cb8915bf
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-05 22:46:47 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240505/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    int sum_val=0;
+    cin >> a >> b;
+
+    for (int i=a;i<=b;i++)
+    {
+        sum_val += i;
+    }
+
+    cout << sum_val;
+
+    return 0;
+}
