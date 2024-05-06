c101f58f0a78d39c47bcfc07f9391b33d6b0986c
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-07 01:56:31 +0900

    Title: 1부터 곱셈하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240507/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int prod=1;
+
+    cin >> n;
+
+    for (int i=1;i<=10;i++)
+    {
+        prod *= i;
+
+        if (prod >= n)
+        {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
