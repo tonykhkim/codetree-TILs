5c7374f4ed676dfacac816fc7cb9bef3c1a642b8
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-06 13:43:20 +0900

    Title: 주어진 수까지의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/주어진 수까지의 곱/multiplication-up-to-a-given-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    int prod = 1;
+    cin >> a >> b;
+
+    for (int i=a;i<=b;i++)
+    {
+        prod *= i;
+    }
+    cout << prod;
+    return 0;
+}
