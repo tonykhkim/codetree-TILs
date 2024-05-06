cdd2a76fc4980485887b7d7128859fa214f02cdc
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-06 13:58:28 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    int prod = 1;
+
+    cin >> a >> b;
+    for (int i = 1;i<=b;i++)
+    {
+        prod *= a;
+    }
+    cout << prod;
+    return 0;
+}
