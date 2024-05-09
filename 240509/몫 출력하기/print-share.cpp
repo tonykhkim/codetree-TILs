61e34f293393592da5a74698439eaf2e4782e3da
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-09 19:57:37 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240509/몫 출력하기/print-share.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int cnt = 0;
+    int n;
+
+    while (true)
+    {
+        cin >> n;
+        
+        if (cnt > 3)
+        {
+            break;
+        }
+
+        if (n%2==0)
+        {
+            cout << n/2 << endl;
+            
+        }
+        cnt += 1;
+    }
+    return 0;
+}
