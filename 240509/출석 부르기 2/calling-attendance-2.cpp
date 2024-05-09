b0fcf18943674286605a79992a8d2b7475ef5288
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-09 19:53:01 +0900

    Title: 출석 부르기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    while(true)
+    {
+        cin >> n;
+
+        if (n==1)
+        {
+            cout << "John" << endl;
+        }
+        else if (n==2)
+        {
+            cout << "Tom" << endl;
+        }
+        else if (n==3)
+        {
+            cout << "Paul" << endl;
+        }
+        else if (n==4)
+        {
+            cout << "Sam" << endl;
+        }
+        else
+        {
+            cout << "Vacancy" << endl;
+            break;
+        }
+    }
+    return 0;
+}
