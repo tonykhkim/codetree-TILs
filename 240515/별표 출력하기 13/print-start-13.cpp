51acdf3ebe52941ffa75d65ef56589d021bc566f
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-15 20:32:45 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240515/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i=1;i<=2*n;i++)
+    {
+        if (i%2==1)
+        {
+            for (int j=1;j<=(n+1)-(i+1)/2;j++)
+            {
+                cout << "* ";
+            }
+            for (int k=1;k<=i/2;k++)
+            {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+}
