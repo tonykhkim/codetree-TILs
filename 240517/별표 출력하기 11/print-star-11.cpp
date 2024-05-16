44ed91298f64658433d2d5a79cd56819c6b88392
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-17 08:45:01 +0900

    Title: 별표 출력하기 11, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240517/별표 출력하기 11/print-star-11.cpp
+++ 240517/별표 출력하기 11/print-star-11.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main()
 {
     int n;
-
     cin >> n;
 
     for (int i=1;i<=2*n+1;i++)
@@ -15,7 +14,7 @@ int main()
             {
                 cout << "* ";
             }
-            else 
+            else
             {
                 cout << "  ";
             }
