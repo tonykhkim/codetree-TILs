f66ab34caa231d18573e54c8fd02bc6ff6eb4bb6
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-17 10:08:34 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240517/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 240517/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -10,7 +10,7 @@ int main()
     {
         for (int j=1;j<=n;j++)
         {
-            if (i==1 || i==6 || j==1 || j==6)
+            if (i==1 || i==n || j==1 || j==n)
             {
                 cout << "* ";
             }
