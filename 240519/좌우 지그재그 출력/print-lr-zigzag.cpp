2e99a6add4622d6ded92c2299ef7e84394fd5a59
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-19 03:06:35 +0900

    Title: 좌우 지그재그 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240519/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i=1;i<=n;i++)
+    {
+        if (i%2==1)
+        {
+            for (int j=n*i-(n-1);j<=n*i;j++)
+            {
+                cout << j << ' ';
+            }
+        }
+        else
+        {
+            for (int j=n*i;j>=n*i-(n-1);j--)
+            {
+                cout << j << ' ';
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
