63cdbfa5340fb06eb9c0e6b9a456583a62e8d1c4
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-19 14:32:03 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240519/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+
+    for (int i=1;i<=9;i++)
+    {
+        for (int j=b;j>=a;j-=2)
+        {
+            cout << j << " * " << i << " = " << j*i;
+
+            if (j>a)
+            {
+                cout << " / ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
