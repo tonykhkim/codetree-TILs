eb70dc7ad361fce965bd671b37a0b2765a10af03
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-19 14:27:09 +0900

    Title: 정상적인 구구단, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240519/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i=1;i<=n;i++)
+    {
+        for (int j=1;j<=n;j++)
+        {
+            cout << i << " * " << j << " = " << i*j;
+
+            if (j<3)
+            {
+                cout << ", ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
