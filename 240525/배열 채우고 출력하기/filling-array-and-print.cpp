e3b501191b56b0f42be09bc536cd42f846447974
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-25 22:10:08 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240525/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[10];
+
+    for (int i=0;i<=9;i++)
+    {
+        cin >> arr[i];
+    }
+    for (int j=9;j>=0;j--)
+    {
+        cout << arr[j];
+    }
+
+    return 0;
+}
