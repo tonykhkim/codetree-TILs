21c951b59e49c426612c9b7cc9398ff70f265cca
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-05-26 22:19:34 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240526/배열 채우기/filling-array.cpp
+++ 240526/배열 채우기/filling-array.cpp
@@ -10,9 +10,17 @@ int main()
     {
         cin >> arr[i];
 
-        if (arr[i]==0 || i==9)
+        if (arr[i]==0)
         {
             for (int j=i-1;j>=0;j--)
+            {
+                cout << arr[j] << " ";
+            }
+            break;
+        }
+        else if (i==9)
+        {
+            for (int j=i;j>=0;j--)
             {
                 cout << arr[j] << " ";
             }
