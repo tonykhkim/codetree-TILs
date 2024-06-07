85f85c7009367c791efd4065b4458e2fdcff81a8
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-06-07 12:35:04 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
+++ 240607/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
-#define MAX_N 1000 // 입력값 N의 최대값
-
-int main() {
-    int n, nums[MAX_N];
-
-    // 입력
+int main()
+{
+    int n;
     cin >> n;
-    for(int i = 0; i < n; i++)
-        cin >> nums[i];
 
-    // 최댓값 찾기
+    int arr[n];
+
+    for (int i = 0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+
     int max = -1;
-    for(int i = 0; i < n; i++) {
-        int curr_num = nums[i];
-        // 최대가 될 수 있는 후보입니다.
-        if(max < curr_num) {
-            // 갱신할 수 있는지 확인하기 위해 이 숫자의 등장 빈도를 셉니다.
+
+    for (int j=0;j<n;j++)
+    {
+        int curr = arr[j];
+
+        if (max < curr)
+        {
             int count = 0;
-            for(int j = 0; j < n; j++) {
-                if(nums[j] == curr_num) {
-                    count++;
+            for (int k = 0;k<n;k++)
+            {
+                if (arr[k] == curr)
+                {
+                    count ++;
                 }
             }
-            // 이 숫자가 배열에서 유일할때만 갱신합니다.
-            if(count == 1) {
-                max = curr_num;
+
+            if (count == 1)
+            {
+                max = curr;
             }
         }
     }
