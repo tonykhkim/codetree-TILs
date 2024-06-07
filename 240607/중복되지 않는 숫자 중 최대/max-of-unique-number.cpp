c88ea9045739c6dcc23331c6fa7fa75a31b1640e
tonykhkim <98434829+tonykhkim@users.noreply.github.com>
2024-06-07 11:27:01 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240607/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
+++ 240607/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -8,35 +8,33 @@ int main()
     cin >> n;
 
     int arr[n];
-    int cnt = 0;
     int max = INT_MIN;
 
-    for (int i=0;i<n;i++)
+    for (int i=0;i<=n;i++)
     {
         cin >> arr[i];
 
         if (arr[i] > max)
         {
-            max = arr[i];
+
+            int count = 0;
+
+            for (int j=0;j<n;j++)
+            {
+                if (arr[j] == arr[i])
+                {
+                    count++;
+                }
+            }
+
+            if (count == 1)
+            {
+                max = arr[i];
+            }
         }
     }
 
-    for (int j=0;j<n;j++)
-    {
-        if (max == arr[j])
-        {
-            cnt += 1;
-        }
-    }
-
-    if (cnt == 1)
-    {
-        cout << max;
-    }
-    else if (cnt >= 2)
-    {
-        cout << -1;
-    }
-    
+    cout << max;
     return 0;
+
 }
