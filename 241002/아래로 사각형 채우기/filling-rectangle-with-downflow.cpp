09916f7c74c73e243d726f9aab4bab9ebe2c7f15
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-02 21:58:21 +0900

    Title: 아래로 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241002/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n][n] = {};
+    int num = 1;
+
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            arr[j][i] = num;
+            num += 1;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
