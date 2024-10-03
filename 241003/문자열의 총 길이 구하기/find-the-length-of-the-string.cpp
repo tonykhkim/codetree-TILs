6e8cab83c0f7725effa18f55478c12e45ca37cfd
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-03 20:12:03 +0900

    Title: 문자열의 총 길이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241003/문자열의 총 길이 구하기/find-the-length-of-the-string.cpp
+++ 241003/문자열의 총 길이 구하기/find-the-length-of-the-string.cpp
@@ -5,14 +5,12 @@ using namespace std;
 int main()
 {
     string arr[10] = {};
-    int count = 0;
     int result = 0;
 
     for (int i=0;i<10;i++)
     {
         cin >> arr[i];
         result += arr[i].length();
-        count += 1;
     }
     cout << result;
 }
