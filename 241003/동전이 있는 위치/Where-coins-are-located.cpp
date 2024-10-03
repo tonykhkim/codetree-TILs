b6cb1e628775c7db21a70569d9d010653b8e166c
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-03 16:09:45 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    int r,c;
+    cin >> n >> m;
+
+    int arr[n][n]={};
+
+    for (int i=0;i<m;i++)
+    {
+        cin >> r >> c;
+        arr[r-1][c-1] = 1;
+    }
+
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
