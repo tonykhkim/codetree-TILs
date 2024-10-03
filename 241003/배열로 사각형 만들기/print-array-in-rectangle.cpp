b630190c222cb1c590cbc4b2e20befe32f7bd8dc
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-03 15:13:47 +0900

    Title: 배열로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/배열로 사각형 만들기/print-array-in-rectangle.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5][5];
+
+    for (int j=0;j<5;j++)
+    {
+        arr[0][j] = 1;
+    }
+
+    for (int i=0;i<5;i++)
+    {
+        arr[i][0] = 1;
+    }
+
+    for (int i=1;i<5;i++)
+    {
+        for (int j=1;j<5;j++)
+        {
+            arr[i][j] = arr[i-1][j] + arr[i][j-1];
+        }
+    }
+
+    for (int i=0;i<5;i++)
+    {
+        for (int j=0;j<5;j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
