3ae086ede8f7088894838b3ba89904514b816e2d
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-04 11:11:59 +0900

    Title: 문자열 추가하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string init, a;
+    cin >> init;
+
+    init.append("Hello");
+    cout << init;
+
+    return 0;
+
+}
