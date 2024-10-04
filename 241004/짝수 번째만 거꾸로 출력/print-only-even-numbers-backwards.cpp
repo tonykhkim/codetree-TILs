3de1208a96dd556500d1e786f2679d8b2a06cb34
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-04 11:07:08 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241004/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.cpp
+++ 241004/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.cpp
@@ -6,7 +6,6 @@ int main()
 {
     string arr;
     cin >> arr;
-    //char A[arr.length()];
     int index = 0;
 
     for (int i=arr.length()-1;i>=0;i--)
@@ -14,15 +13,8 @@ int main()
         if (i%2==1)
         {
             cout << arr[i];
-            //A[index] = arr[i];
             index += 1;
         }
     }
-    //cout << A;
-
-    //for (int i=A.length()-1;i>=0;i--)
-    //{
-    //    cout << A[i];
-    //}
     return 0;
 }
