e8f0bca5d96b608c58c18fcec828992f44463aa8
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-06 01:42:34 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2,num3,num4,num5;
+
+    cin >> num1 >> num2 >> num3 >> num4 >> num5;
+
+    cout << (char)num1 << " " << (char)num2 << " " << (char)num3 << " " << (char)num4 << " " << (char)num5;
+
+    return 0;
+}
