710e2b00ccce5a14d09a2764a1d192ec03cee698
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-06 02:21:04 +0900

    Title: 소문자와 숫자, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/소문자와 숫자/letter-and-number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    int len = str.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        if (isalpha(str[i]))
+        {
+            cout << (char)tolower(str[i]);
+        }
+
+        if (isdigit(str[i]))
+        {
+            cout << str[i];
+        }
+    }
+    return 0;
+}
