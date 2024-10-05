a4d1a8c07ac4b88ce770dc1f6dc7907d0cab95b2
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-06 02:41:58 +0900

    Title: 대문자 소문자 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/대문자 소문자 바꾸기/change-uppercase-and-lowercase.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    int len = str.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        if (isupper(str[i]))
+        {
+            cout << (char)tolower(str[i]);
+        }
+        else
+        {
+            cout << (char)toupper(str[i]);
+        }
+    }
+    return 0;
+}
