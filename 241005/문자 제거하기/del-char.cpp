e417efce83b98d202a36b983ee0690a3fd5dda3f
KyeongHyeon Kim <98434829+tonykhkim@users.noreply.github.com>
2024-10-05 21:13:09 +0900

    Title: 문자 제거하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241005/문자 제거하기/del-char.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    int num,len;
+    while (true)
+    {
+        cin >> num;
+        len = str.length();
+
+        if (len == 1)
+        {
+            break;
+        }
+ 
+        if (num > len)
+        {
+            str.erase(len-1,1);
+        }
+        else
+        {
+            str.erase(num,1);
+        }
+
+        cout << str << endl;
+    }   
+    return 0;
+}
